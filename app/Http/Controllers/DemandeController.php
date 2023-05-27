1a888ae2b868d77b41b305d4b041ba5e1901896a
Mohamed <mohamed.ndoye1@univ-thies.sn>
2023-05-27 15:34:56 +0000

    adding making request to become mentor

--- app/Http/Controllers/DemandeController.php
+++ app/Http/Controllers/DemandeController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Demande;
 use Illuminate\Http\Request;
 
 class DemandeController extends Controller
 {
-    //
+    public function newQuery(Request $request)
+    {
+        $demande = new Demande();
+        $demande->user_id = $request->user()->id;
+        $demande->enabled = false;
+        $demande->save();
+        $request->user()->state = 'pending';
+        $request->user()->save();
+        return response()->json($demande);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DemandeController;
 use App\Http\Controllers\FormationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +23,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/formations', [FormationController::class, 'addFormation']);
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/new-query', [DemandeController::class, 'newQuery']);
 });
 //data: {email, password}
 Route::post('login', [AuthController::class, 'login']);
